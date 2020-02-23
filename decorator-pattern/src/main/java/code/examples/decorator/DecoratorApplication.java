e8054842534db17fffb917034a32bccbc67ea1d1
Igor G F <igor.flakiewicz@gmail.com>
2020-02-23 13:47:27 +0000

    added decorator-pattern module

---
+++ decorator-pattern/src/main/java/code/examples/decorator/DecoratorApplication.java
@@ -0,0 +1,8 @@
+package code.examples.decorator;
+
+public class DecoratorApplication {
+
+    public static void main(String[] args) {
+        System.out.println("In a perfect world...");
+    }
+}
